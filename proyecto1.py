77d3bc2b56992f9feedebace6228ce826e4649ca
Admin <rj.salazara@uea.edu.ec>
2024-02-24 14:49:55 -0500

    ULTIMA VERSION

---
+++ proyecto1.py
@@ -0,0 +1,11 @@
+# Definir la matriz bidimensional (3x3) con valores numéricos
+matriz = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+# Mostrar la matriz
+print("Matriz:")
+for fila in matriz:
+    print(fila)
